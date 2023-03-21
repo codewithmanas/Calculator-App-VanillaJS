4944c2dc5f6d97ffefeec7b8b58cb454ed0022ce
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-03-21 23:28:52 +0530

    basic functionalities added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hi");
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        // console.log(e.target);
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }else if(e.target.innerHTML == "AC"){
+            string = "";
+            document.querySelector("input").value = string;
+        }else if(e.target.innerHTML == "C"){
+                let size = string.length;
+                string = string.substring(0, size-1);
+                document.querySelector("input").value = string;
+        }else {   
+            string = string + e.target.innerHTML;
+            document.querySelector("input").value = string;
+        }
+    })
+})
+
+
+// TODO : Make M+/M- functional
+// TODO : Make any key functional like for (X and *)
+// TODO : Learn Array.from() method
+// TODO : Learn substring() method
